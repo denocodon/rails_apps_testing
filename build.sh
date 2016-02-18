dad58b66e0d8470aa31571fda62108187415259d
denocodon <itrungnguyenthanh@gmail.com>
2016-02-18 10:05:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ source ~/.rvm/scripts/rvm
 type rvm | head -1
 rvm use 2.2.1@default
 bundle install
-rspec hash_rspec.rb --format RspecJunitFormatter --out junut.xml
+rspec hash_rspec.rb --format RspecJunitFormatter --out junit.xml
